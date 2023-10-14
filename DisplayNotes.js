f612f150ec9fba34907e6606983d437ae90bfbd0
NoteMS <elvinmathew35@gmail.com>
2023-10-14 18:32:41 +0530

    replaced the website with major changes after a very long time...this is one of the last commits and contains all the changes made by all the members of this project. after this there would be one final commit before the day of submission/presentation.

--- DisplayNotes.js
+++ DisplayNotes.js
@@ -9,30 +9,11 @@ const publicPath = path.join(__dirname, 'public');
 // Serving files from the 'public' folder
 app.use(express.static(publicPath));
 
-const uploadFilePath = path.join(publicPath,'Files');
-
-let upload = multer({
-    storage:multer.diskStorage({
-        destination : function (req,file,cb){
-            cb(null,uploadFilePath)
-        },
-        filename : function(req,file,cb){
-            cb(null,file.originalname)
-        }
-    })
-}).single("file");
 
 app.get('/display',(rep,res)=>{
     const data = {iframeUrl:'Files/bmp.pdf'} ;
     res.render(`${publicPath}/displayFile.ejs`,data);
 });
 
-app.get('/upload',(rep,res)=>{
-    res.sendFile(`${publicPath}/upload.html`);
-});
-
-app.post('/upload1',upload,async(rep,res)=>{ 
-    res.send('upload successfully');
-});
 
 app.listen(port);
